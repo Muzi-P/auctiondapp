04d140d5b81c16b551420b3c434f6e40de99e56b
Muzi-P <phiwokuhlegondwe@gmail.com>
2019-04-03 01:10:03 +0800

    Added Routes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,11 +13,72 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    .pl-drawer ul {
+    background: none !important;
+}
+.pl-drawer a {
+    color: white !important;
+}
+.pl-drawer i {
+    color: #ffffff !important;
+}
+.is-mobile .pl-intro {
+    padding: 60px 5px 0px 5px;
+    text-align: center;
+}
+.is-mobile .pl-intro h1,
+.is-mobile .pl-intro-2 h1 {
+    font-size: 3em !important;
+    text-shadow: 1px 1px #000;
+}
+.is-mobile .pl-intro h3,
+.is-mobile .pl-intro-2 h3 {
+    font-size: 1.6em !important;
+    text-shadow: 1px 1px #000;
+}
+.is-desktop .pl-intro {
+    padding: 100px;
+    text-align: left;
+}
+.is-mobile .pl-intro-2 {
+    padding: 60px 5px 0px 5px;
+    text-align: center;
+}
+.is-desktop .pl-intro-2 {
+    padding: 100px;
+    text-align: left;
+}
+.pl-darkbg-toolbar {
+    background-color: #1d3446 !important;
+}
+.pl-transparentbg-toolbar {
+    background: none !important;
+    box-shadow: none !important;
+}
+.logo-center-mobile {
+    width: 100% !important;
+}
+.logo-left-desktop {
+    width: auto !important;
+    margin-left: 20px;
+}
+.toolbar__content {
+    height: 100% !important;
+}
+.peer-toolbar .toolbar__side-icon {
+    position: fixed;
+    top: 3px;
+}
+.peer-toolbar a {
+    color: #fff;
+    margin-left: 24px;
+    text-decoration: none;
+    font-size: 1.1em;
+}
+.custom-header {
+    color: #ffffff;
+    font-size: 5em;
+    font-weight: 400;
+}
 }
 </style>
